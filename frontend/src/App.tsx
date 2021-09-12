99ed5bf048196bcf469f4b00b9db2ec93cdf2187
Vivian Barbosa Reis <srtvivian@hotmail.com>
2021-09-11 23:18:00 -0300

    inclusão da imagem do logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NavBar from "components/NavBar";
+
 function App() {
   return (
+    <>
+    <NavBar></NavBar>
     <div>
       <h1 className="text-primary">Olá mundo!</h1>
     </div>
+    </>
   );
 }
 
